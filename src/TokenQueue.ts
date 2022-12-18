346cc3becd533e3cf2f18737d927dc4e6620c70e
Nicholas Hill <nick@nick-hill.com>
2022-12-18 22:42:32 +0000

    Improvements to token queue processing.

--- src/TokenQueue.ts
+++ src/TokenQueue.ts
@@ -10,50 +10,64 @@
             this.position = 0;
         }
 
-        public peek(): Token {
+        public peek(expectedType: number = -1): Token {
             if (this.position >= this.tokens.length) {
                 return null;
             }
-            return this.tokens[this.position];
+            let token = this.tokens[this.position];
+            if (expectedType !== -1 && token.type !== expectedType) {
+                throw 'Unexpected token: \'' + token.value + '\'.';
+            }
+            return token;
         }
 
-        public peekType(): number {
-            var token = this.peek();
-            return token == null ? -1 : token.type;
+        public peekType(expectedType: number = -1): number {
+            let token = this.peek(expectedType);
+            return token === null ? -1 : token.type;
         }
 
-        public peekValue(): string {
-            var token = this.peek();
-            return token == null ? null : token.value;
+        public peekValue(expectedType: number = -1): string {
+            let token = this.peek(expectedType);
+            return token === null ? null : token.value;
         }
 
         public next(expectedType: number = -1): Token {
-            if (this.position >= this.tokens.length) {
-                return null;
-            }
-            var token = this.tokens[this.position];
-            if (expectedType != -1 && token.type != expectedType) {
-                throw 'Unexpected token: \'' + token.value + '\'.';
-            }
+            let token = this.peek(expectedType);
             this.position++;
             return token;
         }
 
         public nextType(expectedType: number = -1): number {
-            var token = this.next(expectedType);
-            return token == null ? -1 : token.type;
+            let token = this.next(expectedType);
+            return token === null ? -1 : token.type;
         }
 
         public nextValue(expectedType: number = -1): string {
-            var token = this.next(expectedType);
-            return token == null ? null : token.value;
+            let token = this.next(expectedType);
+            return token === null ? null : token.value;
         }
 
-        public expect(expectedType: number, expectedValue: string = null) {
-            var token = this.next(expectedType);
-            if (expectedValue != null && token.value != expectedValue) {
-                throw 'Unexpected token value: ' + expectedValue;
+        public expect(expectedType: number, expectedValue: string = null, caseSensitive: boolean = true) {
+            let token = this.next(expectedType);
+            if (expectedValue !== null) {
+                let valueMatches = caseSensitive
+                    ? token.value === expectedValue
+                    : token.value.toLowerCase() === expectedValue.toLowerCase()
+                if (!valueMatches) {
+                    throw 'Unexpected token value: ' + expectedValue;
+                }
             }
+        }
+
+        public expectTypes(...expectedTypes: number[]) {
+            let token = this.next();
+            let tokenType = token?.type ?? -1;
+            for (let expectedType of expectedTypes) {
+                if (tokenType === expectedType) {
+                    return;
+                }
+            }
+            throw 'Unexpected token value: ' + token?.value ?? 'EOF';
         }
 
         public filter(f: (t: Token) => boolean): TokenQueue {
